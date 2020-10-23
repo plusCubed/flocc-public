c5172d2e8b9ad8260db936fc4c24b3509455e146
plusCubed <pluscubed@users.noreply.github.com>
2020-10-23 03:12:11 -0700

    stuff

--- src/renderer/music.js
+++ src/renderer/music.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo, useRef } from 'react';
 import ReactPlayer from 'react-player';
 
 export function Music() {
@@ -17,14 +17,18 @@ export function Music() {
     };
   }, []);
 
+  const playerRef = useRef(null);
+
   return (
-    <div className="py-2 pr-1">
+    <div className="py-2 pr-1 border-t border-solid border-gray-200">
+      <input placeholder="Search music" />
       <ReactPlayer
         playing
         height={100}
         width="100%"
         url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
         config={config}
+        ref={playerRef}
       />
     </div>
   );
