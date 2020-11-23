a30962075c39f3403b7c8b9fd2aa632df90b6b40
plusCubed <pluscubed@users.noreply.github.com>
2020-11-23 06:10:24 -0800

    asdf

--- src/renderer/micPermission.js
+++ src/renderer/micPermission.js
@@ -1,7 +1,7 @@
 import isElectron from 'is-electron';
 
 export async function getOSMicPermissionGranted() {
-  /*if (!isElectron() || require('electron').ipcRenderer.sendSync('is-dev')) {
+  if (!isElectron() || require('electron').ipcRenderer.sendSync('is-dev')) {
     return true;
   }
   const { remote } = require('electron');
@@ -9,6 +9,5 @@ export async function getOSMicPermissionGranted() {
     return await remote.systemPreferences.askForMediaAccess('microphone');
   } else {
     return true;
-  }*/
-  return false;
+  }
 }
